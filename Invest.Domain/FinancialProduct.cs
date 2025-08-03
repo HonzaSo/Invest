9bfc1620a74d4a93be6c146507234bb1c91da394
HonzaSo <jan.sobolciak@gmail.com>
2025-08-03 15:39:45 +0200

    feat: added financialProduct class and integrted to DB context

---
+++ Invest.Domain/FinancialProduct.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class FinancialProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Symbol produktu je vyzadovan.")]
+        public required string Symbol { get; set; }
+        [Required(ErrorMessage = "Datum je povinny.")]
+        //[RegularExpression("((?:19|20)\\d\\d)-(0?[1-9]|1[012])-([12][0-9]|3[01]|0?[1-9])")]
+        public required string From { get; set; }
+
+        public double Open { get; set; }
+
+        public double High { get; set; }
+
+        public double Low { get; set; }
+
+        public double Close { get; set; }
+
+        public Int64 Volume { get; set; }
+    }
+}
--- Invest.Infrastructure/ApplicationDbContext.cs
+++ Invest.Infrastructure/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Invest.Infrastructure
 {
@@ -7,6 +8,16 @@ namespace Invest.Infrastructure
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<FinancialProduct> FinancialProducts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FinancialProduct>().HasData(
+                new FinancialProduct { Id = 1, Symbol = "T", From = "2023-09-01", Open = 153.775, High = 154.33, Low = 150.42, Close = 150.87, Volume = 123456 },
+                new FinancialProduct { Id = 2, Symbol = "E", From = "2023-09-01", Open = 153.775, High = 154.33, Low = 150.42, Close = 150.87, Volume = 123456 },
+                new FinancialProduct { Id = 3, Symbol = "S", From = "2023-09-01", Open = 153.775, High = 154.33, Low = 150.42, Close = 150.87, Volume = 123456 }
+                );
         }
 
     }
